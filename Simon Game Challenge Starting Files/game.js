3c06439f4baf7e2ed1163b02dbaac6acff5a6b1b
Michael Boateng <boatmichael97@gmail.com>
2026-05-24 16:18:19 -0400

    step 4

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,12 +1,27 @@
 // button colors
 var buttonColors = ["green", "red", "yellow", "blue"];
+// game pattern/sequence
+var gamePattern = [];
+// user clicked patter
+var userClickedPattern = [];
 
 // function to randomly pick next tile in the sequence
 function nextSequence(){
     var randonNumber = Math.floor(Math.random() * 4);
     // pick random colour
     var randColor = buttonColors[randonNumber];
+    gamePattern.push(randColor);
+
+    // flash next chosen sequence
+    $('#'+randColor).fadeOut(200).fadeIn(200);
 }
+
+
+$('.btn').on('click', function () {
+    var userChosenColour = $(this).attr("id");
+})
+console.log(userChosenColour);
 
 var num = nextSequence();
 console.log(num);
+
