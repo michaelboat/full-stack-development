3b534962ed09afd6ceae3eeed3c7ecdd38f24c6a
Michael Boateng <boatmichael97@gmail.com>
2026-06-07 14:41:24 -0400

    postman

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -4,8 +4,13 @@ const app = express();
 const port = 1313;
 
 app.get("/", (req, res) => {
-    console.log(req)
+    // console.log(req)
+    res.send("Hello World");
 });
+
+app.get("/contact", (req, res) => {
+    res.send("here's my contact info:")
+})
 
 app.listen(port, () => {
     console.log(`Server has started on port ${port}`)
