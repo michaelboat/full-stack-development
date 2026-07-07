fd6fb4e34eaaa20b536610f7d2b57a75d5daef5f
Michael Boateng <boatmichael97@gmail.com>
2026-07-07 10:29:40 -0400

    mode middleware

---
+++ Backend/3.5 Secrets Project/index.js
@@ -0,0 +1,32 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express"
+import bodyParser from "body-parser"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = 3000;
+const VALIDPASSWORD = "ILoveProgramming";
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+})
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+var userPassword = "";
+app.post("/check", (req, res) => {
+    userPassword = req.body["password"];
+    if (userPassword === VALIDPASSWORD) {
+        res.sendFile(__dirname + "/public/secret.html");
+    }else{
+        res.sendFile(__dirname + "/public/index.html");
+    }
+});
